83f8414b9553b49f6f792c0ecbfde988381c452b
Siddharth Yaduvanshi <siddharth_yadav@outlook.com>
2024-03-14 10:06:45 +0530

    Initial setup /w glfw

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,41 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
+constexpr int WINDOW_WIDTH = 800;
+constexpr int WINDOW_HEIGHT = 600;
+
 int main(int argc, char* argv[])
 {
     (void) argc;
     (void) argv;
 
     std::cout << "Welcome to OpenGL tutorial" << std::endl;
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize glfw" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Learning", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+
+        std::cerr << "Failed to create window" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Rendering Code Here
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
