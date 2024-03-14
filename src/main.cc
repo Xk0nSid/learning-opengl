c558d8ecf1435d2ddb56bd39b6bde5cf25077bf5
Siddharth Yaduvanshi <siddharth_yadav@outlook.com>
2024-03-14 10:51:58 +0530

    Base OpenGL /w glew & glfw setup

--- src/main.cc
+++ src/main.cc
@@ -37,6 +37,13 @@ int main(int argc, char* argv[])
 
     while (!glfwWindowShouldClose(window)) {
         // Rendering Code Here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
 
         glfwSwapBuffers(window);
 
